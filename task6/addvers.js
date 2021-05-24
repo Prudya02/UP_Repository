a15fd1cfb3e38454dc85319da60fa06988f5caf9
Vladislav <prudnikovich61@gmail.com>
2021-05-24 17:13:27 +0300

    added task6

---
+++ task6/addvers.js
@@ -0,0 +1,549 @@
+adList =  [
+    {
+
+        id: '1',
+        description: 'Курсы по java',
+        createdAt: new Date('2021-04-23T24:00:00'),
+        siteLink: 'https://teachmeskills.by/',
+        vendor: 'TeachMeSkills',
+        photoLink: 'pic/p1.jpg',
+        hashTags: ['java','courses', 'allSummer'],
+        discount: '15%',
+        validUntil: new Date('2021-06-01T24:00:00'),
+        rating: '4.7',
+        reviews: ['Можно ли присоединиться к курсу в середине лета?']
+    },
+
+    {
+
+        id: '2',
+        description: 'IT-Инкубатор',
+        createdAt: new Date('2021-04-05T22:10:00'),
+        siteLink: 'https://it-incubator.by',
+        vendor: 'ITINCUBATOR',
+        photoLink: 'pic/p2.png',
+        hashTags: ['incub', 'front', 'ReactJS'],
+        discount: '10%',
+        validUntil: new Date('2021-09-01T22:10:00'),
+        rating: '4.5',
+        reviews: ['Порадовала возможность оплаты только полсе части обучения']
+
+    },
+    {
+
+        id: '3',
+        description: 'Курс Python разработчик',
+        createdAt: new Date('2021-04-01T10:30:00'),
+        siteLink: 'https://teachmeskills.by',
+        vendor: 'TeachMeSkillsa',
+        photoLink: 'pic/p3.png',
+        hashTags: ['python', 'allSummer'],
+        discount: '15%',
+        validUntil: new Date('2021-06-01T23:00:00'),
+        rating: '4.0',
+        reviews: ['Приятная атмосфера','Много воды...']
+
+    },
+    {
+
+        id: '4',
+        description: 'Интенсив «Язык C++: создаём сервер за 3 дня»',
+        createdAt: new Date('2021-01-01T21:00:00'),
+        siteLink: 'https://tproger.ru',
+        vendor: 'Tproger',
+        photoLink: 'p4.png',
+        hashTags: ['c++', 'server', 'intensive'],
+        discount: '30%',
+        validUntil: new Date('2021-07-01T21:00:00'),
+        rating: '4.5',
+        reviews: ['Полезные знания, однако без скидки слишком дорого)']
+
+    },
+    {
+
+        id: '5',
+        description: 'Java для начинающих',
+        createdAt: new Date('2021-04-04T19:00:00'),
+        siteLink: 'https://www.udemy.com/course/beginners-java/',
+        vendor: 'Udemy',
+        photoLink: 'p5.jpg',
+        hashTags: ['java', 'forBeginners'],
+        discount: '89%',
+        validUntil: new Date('2021-10-01T23:00:00'),
+        rating: '4.6',
+        reviews: ['Немного накрученная цена, но материал хороший']
+
+    },
+    {
+
+        id: '6',
+        description: 'Java.От простого к сложному.',
+        createdAt: new Date('2021-04-04T20:00:00'),
+        siteLink: 'https://www.udemy.com/course/java-simple2advanced/',
+        vendor: 'Udemy',
+        photoLink: 'pic/p6.jpg',
+        hashTags: ['java', 'learnMore'],
+        discount: '67%',
+        validUntil: new Date('2021-10-01T23:00:00'),
+        rating: '4.7',
+        reviews: ['Одна только часть про многопоточность отбивает стоимость курса)']
+
+    },
+    {
+
+        id: '7',
+        description: 'iOS-разработчик',
+        createdAt: new Date('2021-09-01T23:00:00'),
+        siteLink: 'https://teachmeskills.by/',
+        vendor: 'TeachMeSkills',
+        photoLink: 'pic/p7.jpg',
+        hashTags: ['iOS, swift'],
+        discount: '20%',
+        validUntil: new Date('2022-09-01T23:00:00'),
+        rating: '4.5',
+        reviews: ['Хороший курс!']
+
+    },
+    {
+
+        id: '8',
+        description: 'Онлайн-Курс Аналитик Данных',
+        createdAt: new Date('2020-02-01T20:00:00'),
+        siteLink: 'https://skillfactory.ru/data-analyst-pro',
+        vendor: 'SkillFactory',
+        photoLink: 'p8.jpg',
+        hashTags: ['sql', 'analyst'],
+        discount: '15%',
+        validUntil: new Date('2020-08-01T12:00:00'),
+        rating: '4.8',
+        reviews: ['Отличный курс, нашёл после него работу!']
+
+    },
+    {
+
+        id: '9',
+        description: 'Введение в Java',
+        createdAt: new Date('2020-12-01T12:00:00'),
+        siteLink: 'https://ru.hexlet.io/courses/java_101',
+        vendor: 'Хекслет',
+        photoLink: 'p8.jpg',
+        hashTags: ['java', 'forBeginners'],
+        discount: '15%',
+        validUntil: new Date('2021-12-01T12:00:00'),
+        rating: '4.0',
+        reviews: ['Очень много воды...']
+
+    },
+    {
+
+        id: '10',
+        description: 'Learn To Program with Pascal',
+        createdAt: new Date('2021-01-28T11:05:30'),
+        siteLink: 'https://www.udemy.com/course/learn-to-program-with-pascal/',
+        vendor: 'Udemy',
+        photoLink: 'pic/p10.jpg',
+        hashTags: ['Pascal', 'pro'],
+        discount: '89%',
+        validUntil: new Date('2021-09-01T23:00:00'),
+        rating: '4.5',
+        reviews: ['Хороший курс, объясняющий сложные вещи простым языком!']
+
+    },
+    {
+
+        id: '11',
+        description: 'Обучение Java c нуля.',
+        createdAt: new Date('2020-08-13T12:00:00'),
+        siteLink: 'https://javarush.ru',
+        vendor: 'JavaRush',
+        photoLink: 'pic/p11.jpg',
+        hashTags: ['java', 'easy'],
+        discount: '25%',
+        validUntil: new Date('2021-09-01T12:00:00'),
+        rating: '4.0',
+        reviews: ['Довольно десткая форма обучения','Мало практических задач']
+
+    },
+    {
+
+        id: '12',
+        description: 'Jet Brains academy 1 year acsess',
+        createdAt: new Date('2020-12-10T9:00:00'),
+        siteLink: 'https://www.jetbrains.com/ru-ru/academy/',
+        vendor: 'JetBrains',
+        photoLink: 'pic/p12.jpg',
+        hashTags: ['JBA', 'Java','Python'],
+        discount: '55%',
+        validUntil: new Date('2021-12-01T9:00:00'),
+        rating: '5.0',
+        reviews: ['Лучшие курсы по программированию','Однозначно стоит своих денег']
+
+    },
+    {
+
+        id: '13',
+        description: 'Профессия Frontend-разработчик pro',
+        createdAt: new Date('2021-05-10T21:00:00'),
+        siteLink: 'https://skillbox.ru/course/by/frontend-pro/',
+        vendor: 'Skillbox',
+        photoLink: 'pic/p13.jpg',
+        hashTags: ['front', 'pro', 'ReactJS'],
+        discount: '30%',
+        validUntil: new Date('2022-05-10T21:00:00'),
+        rating: '4.3',
+        reviews: ['Неплохой материал, но рассказчик унылый(']
+
+    },
+    {
+
+        id: '14',
+        description: 'Курс “Ruby on Rails (веб-разработка)”',
+        createdAt: new Date('2021-03-02T24:00:00'),
+        siteLink: 'https://www.it-courses.by/courses/ruby-on-rails-web-development/',
+        vendor: 'StormNet',
+        photoLink: 'pic/p14.jpg',
+        hashTags: ['Ruby', 'RoR'],
+        discount: '45%',
+        validUntil: new Date('2022-03-01T24:00:00'),
+        rating: '4.0',
+        reviews: []
+
+    },
+    {
+
+        id: '15',
+        description: 'Профессия 1C-разработчик',
+        createdAt: new Date('2015-05-15T04:00:00'),
+        siteLink: 'https://skillbox.ru/course/by/profession-1c/?',
+        vendor: 'Skillbox',
+        photoLink: 'pic/p15.jpg',
+        hashTags: ['1С', 'online'],
+        discount: '40%',
+        validUntil: new Date('2025-05-24T23:00:00'),
+        rating: '4.6',
+        reviews: ['Спокойно нашёл работу после этого курса']
+
+    },
+    {
+
+        id: '16',
+        description: 'Курс “Программирование Python/Django”',
+        createdAt: new Date('2020-11-18T03:00:00'),
+        siteLink: 'https://www.it-courses.by/courses/programming-with-python/',
+        vendor: 'StormNet',
+        photoLink: 'pic/p16.jpg',
+        hashTags: ['Py', 'Django'],
+        discount: '45%',
+        validUntil: new Date('2021-11-01T23:00:00'),
+        rating: '3.5',
+        reviews: ['Недостаточно практики за такие деньги']
+
+    },
+    {
+
+        id: '17',
+        description: 'Промышленное программирование на Java',
+        createdAt: new Date('2021-05-10T10:00:00'),
+        siteLink: 'https://www.it-courses.by/courses/programming-with-python/0',
+        vendor: 'IT-ACADEMY',
+        photoLink: 'pic/p17.jpg',
+        hashTags: ['Java', 'pro'],
+        discount: '10%',
+        validUntil: new Date('2022-05-01T13:00:00'),
+        rating: '0.0',
+        reviews: []
+
+    },
+    {
+
+        id: '18',
+        description: 'Разработка на С#',
+        createdAt: new Date('2020-04-02T12:00:00'),
+        siteLink: 'https://gb.ru/geek_university/csharp-developer',
+        vendor: 'GeekBrains',
+        photoLink: 'pic/p18.jpg',
+        hashTags: ['CSharp', 'Web'],
+        discount: '40%',
+        validUntil: new Date('2020-09-01T12:00:00'),
+        rating: '4.8',
+        reviews: ['Помогают с трудоустройством после обучения']
+
+    },
+    {
+
+        id: '19',
+        description: 'Веб-разработчик с нуля',
+        createdAt: new Date('2021-01-12T10:00:00'),
+        siteLink: 'https://netology.ru/programs/web-developer',
+        vendor: 'Netology',
+        photoLink: 'p19.jpg',
+        hashTags: ['Web', 'forBeginners'],
+        discount: '40%',
+        validUntil: new Date('2021-06-01T23:00:00'),
+        rating: '4.8',
+        reviews: ['Достойный курс за свои деньги']
+
+    },
+    {
+
+        id: '20',
+        description: 'Основы программирования на Python',
+        createdAt: new Date('2021-02-01T02:00:00'),
+        siteLink: 'https://www.instituteiba.by/courses/it/python/python-courses/the-basics-of-programming-in-python/',
+        vendor: 'IBA_INSTITUTE',
+        photoLink: 'pic/p20.jpg',
+        hashTags: ['Python', 'IBA'],
+        discount: '15%',
+        validUntil: new Date('2021-08-01T02:00:00'),
+        rating: '4.8',
+        reviews: ['Много практики, одобряю)']
+    },
+];
+
+function compareData(a, b) {
+    return new Date(a.createdAt) - new Date(b.createdAt);
+}
+
+function getAds(skip, top, filterConfig) {
+
+    if (typeof skip === 'number' && typeof top === 'number' && skip >= 0 && top > 0) {
+
+        var list;
+        var list1;
+        var list2;
+
+        if (!filterConfig) {
+            list = adList.sort(compareData);
+        } else if (typeof filterConfig === 'object') {
+            if (filterConfig.vendor) {
+                list1 = adList.filter(function (element) {
+                    return element.vendor === filterConfig.vendor;
+                });
+
+            }
+            if (filterConfig.hashTags) {
+                list2 = adList.filter(function (element) {
+                    return element.hashTags.some(function (elem) {
+                        return filterConfig.hashTags.includes(elem);
+                    });
+                });
+            }
+                if(list1&&list2){
+                    list = list1.concat(list2);
+                }
+                else if(list2){
+                    list = Object.assign(list2);
+                }else{
+                    list=Object.assign(list1);
+                }
+
+                list = list.sort(compareData);
+        } else {
+            return null;
+        }
+
+        return list.slice(skip, top + skip);
+    }
+
+    return null;
+}
+
+console.log('getAds(0, 6)', getAds(0, 6));
+console.log('getAds(2, 6)', getAds(2, 6));
+console.log('getAds(0, 6, {vendor: \'TeachMeSkills\'})', getAds(0, 6, {vendor: 'TeachMeSkills'}));
+console.log('getAds(2, 5, {vendor: \'SkillBox\'})', getAds(2, 5, {vendor: 'SkillBox'}));
+console.log('getAds(10, 10, {hashTags: [\'Web\',\'java\']})', getAds(10, 20, {hashTags: ['Web','java']}));
+console.log('getAds(2, 5, {hashTags: [\'chairs\'})', getAds(0, 5, {hashTags: ['chairs']}));
+console.log('getAds(0, 8, 10)', getAds(0, 8, 10));
+console.log('getAds(\'a\', \'b\')', getAds('a', 'b'));
+console.log('getAds(10, 0)', getAds(10, 0));
+console.log('getAds()', getAds());
+
+function getAd(id) {
+
+    var item;
+    if (typeof id === 'string') {
+        item = adList.find(function (element) {
+            return element.id === id;
+        });
+        if (item) {
+            return item;
+        }
+    }
+
+    return false;
+}
+
+console.log('getAd(\'3\')', getAd('3'));
+console.log('getAd(\'100\')', getAd('100'));
+console.log('getAd(\'0\')', getAd('0'));
+console.log('getAd(1)', getAd(1));
+console.log('getAd(\'a\'))', getAd('a'));
+console.log('getAd()', getAd());
+
+function validateAd(adItem) {
+
+    if (typeof adItem === 'object') {
+
+        return typeof adItem.id === 'string' && (adItem.id > 0 || adItem.id <= adList.length) &&
+            typeof adItem.description === 'string' && 
+            adItem.description !== '' && adItem.description.length <= 200 &&
+            typeof adItem.createdAt === 'object' && adItem.createdAt !== null &&
+            typeof adItem.vendor === 'string' && adItem.vendor !== '' &&
+            typeof adItem.hashTags === 'object' && adItem.hashTags !== null && adItem.hashTags.length <= 7 &&
+            adItem.hashTags.every(function (item) {
+                return !(typeof item !== 'string' && item.length >= 20);
+            }) &&
+            typeof adItem.discount === 'string' && adItem.discount !== '' && adItem.discount.length <= 4;//скидка меньше 100%
+    }
+    return false;
+}
+console.log('validateAd(getAd(\'4\'))', validateAd(getAd('4')));
+tempItem = {
+
+    id: '5',
+    description: 'Курс 1',
+    createdAt: new Date('2021-01-01T23:14:00'),
+    siteLink: 'https://course1.by',
+    vendor: 'vendor1',
+    photoLink: 'pic/p8.jpg',
+    hashTags: ['курс!'],
+    discount: '100%',
+    validUntil: new Date('2021-04-21T12:00:00'),
+    rating: '5.0',
+    reviews: ['Действительно, Курс!']
+
+};
+
+function addAd(adItem) {
+
+    if (validateAd(adItem)) {
+        adList.push(adItem);
+        return true;
+    }
+
+    return false;
+}
+
+console.log('validateAd(', tempItem, ')', validateAd(tempItem));
+console.log('validateAd(0)', validateAd(0));
+console.log('validateAd(\'a\')', validateAd('a'));
+console.log('validateAd()', validateAd());
+
+tempItem2 = {
+
+     id: '5',
+    description: 'Курс 1',
+    createdAt: new Date('2021-01-01T23:14:00'),
+    siteLink: 'https://course1.by',
+    vendor: 'vendor1',
+    photoLink: 'pic/p8.jpg',
+    hashTags: ['курс!!!!!!!!!'],
+    discount: '100%',
+    validUntil: new Date('2021-04-21T12:00:00'),
+    rating: '5.0',
+    reviews: ['Действительно, Курс!']
+
+};
+
+console.log('addAd(', tempItem2, ')', addAd(tempItem2));
+console.log('addAd(\'item\')', addAd('item'));
+console.log('addAd(1)', addAd(1));
+console.log('addAd()', addAd());
+
+function editAd(id, adItem) {
+
+    if (typeof id === 'string' && id > 0 && id <= adList.length && typeof adItem === 'object') {
+
+        var copyItem = Object.assign({}, getAd(id));
+
+        if (adItem.id || adItem.createdAt || adItem.vendor) {
+            return false;
+        } 
+
+        if (adItem.description) {
+            copyItem.description = adItem.description;
+        }
+
+        if (adItem.siteLink) {
+            copyItem.siteLink = adItem.siteLink;
+        }
+
+        if (adItem.photoLink) {
+            copyItem.photoLink = adItem.photoLink;
+        }
+
+        if (adItem.hashTags) {
+            copyItem.hashTags = adItem.hashTags;
+        }
+
+        if (adItem.discount) {
+            copyItem.discount = adItem.discount;
+        }
+
+        if (adItem.validUntil) {
+            copyItem.validUntil = adItem.validUntil;
+        }
+
+        if (adItem.rating) {
+            copyItem.rating = adItem.rating;
+        }
+
+        if (adItem.reviews) {
+            copyItem.reviews = adItem.reviews;
+        }
+
+        if (validateAd(copyItem)) {
+            adList.splice(id-1, 1, copyItem);
+
+            return true;
+        }
+    }
+
+    return false;
+}
+
+console.log(getAd('1'));
+
+console.log('editAd(\'1\', {discount: \'10%\'})',
+    editAd('1', {discount: '10%'}));
+
+console.log('editAd(\'1\', {hashTags: new Array(\'hashTag\')}',
+    editAd('1', {hashTags: new Array('hashTag')}));
+
+console.log(getAd('1'));
+
+console.log('editAd(1, {discount: \'69%\'}',
+    editAd(1, {discount: '69%'}));
+
+console.log('editAd(\'1\')',
+    editAd('1'));
+
+console.log('editAd()',
+    editAd());
+
+console.log(getAd('1'));
+
+function removeAd(id) {
+
+    if (typeof id === 'string' && id > 0 && id <= adList.length) {
+
+        var item;
+        var index = adList.findIndex(function (element) {
+            return element.id === id;
+        })
+        if (index !== -1) {
+            adList.splice(index, 1);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+console.log('getAd(\'21\')', getAd('21'));
+console.log('removeAd(\'21\')', removeAd('21'));
+console.log('getAd(\'21\')', getAd('21'));
+console.log('removeAd(1)', removeAd(1));
+console.log('removeAd(\'a\')', removeAd('a'));
+console.log('removeAd()', removeAd());
